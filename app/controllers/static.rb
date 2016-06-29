12709ceacb241246d20d579623c0f40ce47d7a79
random <liewyj619@gmail.com>
2016-06-29 11:21:33 +0800

    first app bit.ly

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,32 +1,31 @@
 require 'byebug'
-require 'byebug'
+
 get '/' do
 	@url = nil
   erb :"static/index"
 end
 
 post '/urls' do
-	unless Url.exists?(long_url: params[:long_url])
-  		@url = Url.create(:long_url => params[:long_url], :short_url => Url.shorten)
-  		
-  	else 
-  		Url.all.each do |u| 
-  			if u.long_url == params[:long_url] 
-  				@url = u
-  				break
-  			end
-  		end
-  	end
-  	 erb :"static/index"
+  byebug
+  if params[:long_url].valid?
+  	unless Url.exists?(long_url: params[:long_url])
+    		@url = Url.create(:long_url => params[:long_url], :short_url => Url.shorten, :click_count => 0)
+    	else 
+    		Url.all.each do |u| 
+    			if u.long_url == params[:long_url] 
+    				@url = u
+    				break
+    			end
+    		end
+    	end
+    	 erb :"static/index"
+  else 
+    redirect '/'
+  end
 end
 
 get '/:short_url' do
 	@url = Url.find_by(short_url: params[:short_url])
+	@url.add_click_count
 	redirect 'http://' + @url.long_url
 end
-
-# post '/urls' do
-# 	Url.create(long_url: params[:long_url], short_url: Url.shorten)
-#   erb :"static/index"
-#   redirect '/'
-# end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,18 @@
+require 'uri'
+
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	valid_url_input = /http?:\/\/[\S]+/
+	validates_of_presence :long_url
+	validates_format_of :long_url, :with => valid_url_input 
+
+
 	def self.shorten
 		(0..7).map {[(65 + rand(26)).chr, (97 + rand(26)).chr, rand(10)].sample(1) }.join
+	end
+
+	def add_click_count
+		self.click_count += 1
+		self.save!
 	end
 end
