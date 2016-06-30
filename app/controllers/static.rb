cce8e4405ed9079c7ceeddd7cf18f759a04cc36d
random <liewyj619@gmail.com>
2016-06-30 09:20:10 +0800

    ....

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,31 +1,38 @@
 require 'byebug'
 
 get '/' do
-	@url = nil
+	@url = Url.all
+  @msg = ""
   erb :"static/index"
 end
 
 post '/urls' do
-  byebug
-  if params[:long_url].valid?
-  	unless Url.exists?(long_url: params[:long_url])
-    		@url = Url.create(:long_url => params[:long_url], :short_url => Url.shorten, :click_count => 0)
-    	else 
-    		Url.all.each do |u| 
-    			if u.long_url == params[:long_url] 
-    				@url = u
-    				break
-    			end
-    		end
-    	end
-    	 erb :"static/index"
-  else 
-    redirect '/'
+  @msg  = ""
+	unless Url.exists?(long_url: params[:long_url]) 
+    @url = Url.new(:long_url => params[:long_url], :short_url => Url.shorten)
+    @url.save ? (@msg = "successfully saved") : (@msg = "URL invalid")
   end
+  @url = Url.all
+  erb :"static/index"
 end
 
 get '/:short_url' do
 	@url = Url.find_by(short_url: params[:short_url])
-	@url.add_click_count
-	redirect 'http://' + @url.long_url
+	@url.increment(:click_count)
+  @url.save
+	redirect @url.long_url
 end
+
+
+# @url = url.find_by(:long_url: params[:long_url])
+# if @urlredierect '/'
+# else
+#   @url = Url.new(:long_url => params[:long_url])
+#   if @url.save <----check
+#       @msg = "successfully saved"
+#       redirect "/"
+#       else
+#         @msg = "failed"
+#         redirect "/"
+
+# @msg = "#{@url.errors.full_messages.join(" ")}"
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,17 +2,22 @@ require 'uri'
 
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-	valid_url_input = /http?:\/\/[\S]+/
-	validates_of_presence :long_url
-	validates_format_of :long_url, :with => valid_url_input 
-
+	validates_presence_of :long_url
+	validates :long_url, format:{ with: (URI::regexp(['http', 'https']))}
 
 	def self.shorten
-		(0..7).map {[(65 + rand(26)).chr, (97 + rand(26)).chr, rand(10)].sample(1) }.join
-	end
-
-	def add_click_count
-		self.click_count += 1
-		self.save!
+		short_url = (0..7).map {[(65 + rand(26)).chr, (97 + rand(26)).chr, rand(10)].sample(1) }.join
 	end
 end
+
+
+
+# active record call back method
+# before_save :shorten_it
+# can also do after_save
+
+# def shorten_it
+# 	sef.short_irl = SecureRAndom.hex(6)
+# end
+
+# validatio: :long_url,  format: {with: URI::regex("http", "https")}
